b49b00d896066321314fda5bb64a155cf113e479
Alporokh <87474029+Alporokh@users.noreply.github.com>
2026-03-31 21:00:08 +0000

    Deploying to gh-pages from @ Alporokh/goit-advancedjs-hw-01@633d9952a25745e38dab5f025fd24a575643ffac 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,2 +1,2 @@
-/* empty css                      */import"./assets/modulepreload-polyfill-BnkOoLKg.js";console.log(`Form`);
+/* empty css                      */import"./assets/modulepreload-polyfill-wMinxHhO.js";document.querySelector(`.feedback-form`);
 //# sourceMappingURL=2-form.js.map
