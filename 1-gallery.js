0dbc4f041af8c0b92643905848b516dfbcab656c
Alporokh <87474029+Alporokh@users.noreply.github.com>
2026-03-31 13:09:02 +0000

    Deploying to gh-pages from @ Alporokh/goit-advancedjs-hw-01@b5eb56e4cf38177b53337951197797523a4948a9 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+/* empty css                      */import"./assets/modulepreload-polyfill-BnkOoLKg.js";console.log(`Gallery`);
+//# sourceMappingURL=1-gallery.js.map
---
+++ 2-form.js
@@ -0,0 +1,2 @@
+/* empty css                      */import"./assets/modulepreload-polyfill-BnkOoLKg.js";console.log(`Form`);
+//# sourceMappingURL=2-form.js.map
